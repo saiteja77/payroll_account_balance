f1e21d203fc1875632f779a8b8ddfb93d7ab2a0e
Sai Teja Kommuri <32244803+saiteja77@users.noreply.github.com>
2022-04-04 11:57:14 -0500

    initial commit

---
+++ src/main/java/com/bitbyte/payroll_account_balance/PayrollAccountBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.bitbyte.payroll_account_balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollAccountBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollAccountBalanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitbyte/payroll_account_balance/PayrollAccountBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitbyte.payroll_account_balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollAccountBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
